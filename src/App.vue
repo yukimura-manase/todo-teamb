49e07a59bac3eca0fa9561bded149f77c4c78ae4
s4k4shun <s4k4shunkunn@gmail.com>
2021-08-05 17:35:20 +0900

    ホーム画面作成完了

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/Todolist">TODO一覧</router-link> |
+      <router-link to="/Todopush">TODO追加画面</router-link>
     </div>
     <router-view/>
   </div>
