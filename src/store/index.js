17b60ddf74742d8e7eac535ed890ce67faa28e67
yukimura-manase <masahiro19951212@gmail.com>
2021-08-06 10:07:35 +0900

    Storeを更新しました

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos:[]
+
   },
   mutations: {
+    todoPush(state,todo){
+      state.todos.push(todo);
+      console.log('todoPush完了');
+      console.log(state.todos);
+    }
+
   },
   actions: {
+
+    todoPush({commit},todo){
+      console.log('todoPush起動！！');
+      commit('todoPush',todo);
+    }
+
   },
   modules: {
+
   }
 })
